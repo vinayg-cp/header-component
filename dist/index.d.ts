7428e554c947a503191e2fae530b77ae2e04994a
Vinay Gopalaiah <vinay.g@codeparva.in>
2021-03-24 17:00:19 +0530

    To publish demo package

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+declare const Navbar: () => JSX.Element;
+export default Navbar;
---
+++ dist/index.js
@@ -0,0 +1,52 @@
+
+
+function ___$insertStyle(css) {
+  if (!css) {
+    return;
+  }
+  if (typeof window === 'undefined') {
+    return;
+  }
+
+  var style = document.createElement('style');
+
+  style.setAttribute('type', 'text/css');
+  style.innerHTML = css;
+  document.head.appendChild(style);
+  return css;
+}
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var React = require('react');
+
+function _interopNamespace(e) {
+    if (e && e.__esModule) return e;
+    var n = Object.create(null);
+    if (e) {
+        Object.keys(e).forEach(function (k) {
+            if (k !== 'default') {
+                var d = Object.getOwnPropertyDescriptor(e, k);
+                Object.defineProperty(n, k, d.get ? d : {
+                    enumerable: true,
+                    get: function () {
+                        return e[k];
+                    }
+                });
+            }
+        });
+    }
+    n['default'] = e;
+    return Object.freeze(n);
+}
+
+var React__namespace = /*#__PURE__*/_interopNamespace(React);
+
+var Navbar = function () {
+    return (React__namespace.createElement(React__namespace.Fragment, null,
+        React__namespace.createElement("div", null, " Hello Navbar "),
+        React__namespace.createElement("p", null, "Adding demo content")));
+};
+
+exports.default = Navbar;
+//# sourceMappingURL=index.js.map
